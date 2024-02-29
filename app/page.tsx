6f466052704a843a653856702a0ba7dab0b6e98f
haris <haris.fadhilah21@gmail.com>
2024-02-29 18:07:42 +0700

    style: change asset in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -49,13 +49,13 @@ export default function Home() {
           <div className="sm:flex sm:flex-wrap justify-center container sm:mb-0">
             <div className="bg-white rounded-2xl mx-auto text-center py-10 px-8 w-4/5 sm:w-2/5 mb-10">
               <h2 className='text-primary-700 text-xl font-semibold mb-5'>Solution for retail</h2>
-              <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={500} height={500} className='object-cover mx-auto w-1/2 mb-5' />
+              <Image src={'/assets/solution_retail.webp'} alt={'solution for retail'} width={500} height={500} className='object-cover mx-auto w-1/2 mb-5' />
               <p className='mb-8 text-neutral-700'> Elevate your everyday. Experience seamless internet, captivating TV, and clear phone calls with Ideanet personal solutions.</p>
               <a href='#' className='text-primary font-bold'>More info ❯</a>
             </div>
             <div className="bg-white rounded-2xl mx-auto text-center py-10 px-8 w-4/5 sm:w-2/5 mb-10">
               <h2 className='text-primary-700 text-xl font-semibold mb-5'>Solution for enterprise</h2>
-              <Image src={'/assets/solution_enterprise.jpeg'} alt={'solution for enterprise'} width={500} height={500} className='object-cover w-1/2  mx-auto mb-5' />
+              <Image src={'/assets/solution_enterprise.webp'} alt={'solution for enterprise'} width={500} height={500} className='object-cover w-1/2  mx-auto mb-5' />
               <p className='mb-8 text-neutral-700'> In today&apos;s fast-paced world, your IT infrastructure needs to be agile and secure. Ideanet enterprise solutions provide the foundation for success.</p>
               <a href='#' className='text-primary font-bold'>More info ❯</a>
             </div>
@@ -66,32 +66,32 @@ export default function Home() {
 
           <div className="sm:flex sm:flex-wrap justify-center container sm:mb-0 gap-6">
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
-              <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+              <div className="w-full px-7 absolute -top-1/3 left-0">
+                <Image src={'/assets/home_why1.webp'} alt={'save money'} width={120} height={120} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Save money</h6>
               <p className=' text-neutral-700 text-sm'> Affordable maintenance keeps your budget balanced, freeing up resources for growth.</p>
             </div>
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
-              <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+              <div className="w-full px-7 absolute -top-1/3 left-0">
+                <Image src={'/assets/home_why2.webp'} alt={'boost productivity'} width={120} height={120} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Boost productivity</h6>
               <p className=' text-neutral-700 text-sm'> Our solutions streamline operations, helping you work smarter and achieve more.</p>
             </div>
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
-              <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+              <div className="w-full px-7 absolute -top-1/3 left-0">
+                <Image src={'/assets/home_why3.webp'} alt={'security you can trust'} width={120} height={120} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Security you can trust</h6>
               <p className=' text-neutral-700 text-sm'> Flexible, scalable technology enhances security and protects your valuable data.</p>
             </div>
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
-              <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+              <div className="w-full px-7 absolute -top-1/3 left-0">
+                <Image src={'/assets/home_why4.webp'} alt={'solution for retail'} width={120} height={120} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Focus on your business</h6>
               <p className=' text-neutral-700 text-sm'> We handle IT headaches, so you can focus on what you do best – run and grow your enterprise.</p>
@@ -99,15 +99,15 @@ export default function Home() {
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
               <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+                <Image src={'/assets/home_why5.webp'} alt={'always connected'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
-              <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Never unplug</h6>
+              <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Always connected</h6>
               <p className=' text-neutral-700 text-sm'> 24/7 customer support ensures you&apos;re always connected and never left in the dark.</p>
             </div>
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
               <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+                <Image src={'/assets/home_why6.webp'} alt={'experience the speed'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Experience the speed</h6>
               <p className=' text-neutral-700 text-sm'> Blazing-fast internet puts you ahead of the game and streamlines every digital task.</p>
@@ -115,7 +115,7 @@ export default function Home() {
 
             <div className="relative bg-white rounded-2xl mx-auto text-center pb-10 pt-16 px-8 w-4/5 sm:w-64 md:w-72 mb-20">
               <div className="w-full px-7 absolute -top-1/4 left-0">
-                <Image src={'/assets/solution_retail.jpeg'} alt={'solution for retail'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
+                <Image src={'/assets/home_why7.webp'} alt={'experts at your side'} width={100} height={100} className=' -top-1/2 left-1/2  object-cover mx-auto  mb-5 rounded-2xl' />
               </div>
               <h6 className='text-gray-900 font-bold text-sm mb-2.5'>Experts at your side</h6>
               <p className=' text-neutral-700 text-sm'> Our seasoned team brings experience and expertise to guide you every step of the way.</p>
@@ -131,8 +131,8 @@ export default function Home() {
 
             <h2 className='text-primary-700 text-xl font-semibold mb-5 text-center sm:text-left'>Internet Service</h2>
             <div className="md:grid md:grid-cols-2 mb-5">
-              <Image src={'/assets/home_product1.png'} alt={'solution for retail'} width={500} height={600} className='object-cover mx-auto w-3/4 mb-5 sm:order-2' />
-              <div id='cardproduct' className="bg-white rounded-2xl py-10 px-8 w-4/5 sm:w-full mb-10 sm:order-1 mx-auto shadow-[0_0_10px_3px_rgba(229,229,229)] h-min">
+              <Image src={'/assets/home_product1.webp'} alt={'solution for retail'} width={500} height={600} className='object-cover mx-auto w-3/4 mb-5 sm:order-2' />
+              <div id='cardproduct' className="bg-white rounded-2xl py-10 px-8 w-4/5 sm:w-full md:mt-10 mb-10 sm:order-1 mx-auto shadow-[0_0_10px_3px_rgba(229,229,229)] h-min">
                 <p className='mb-5 text-neutral-700 text-sm'><b>1. Home or office,</b> From casual browsing to powering business empires, our internet plans ignite your digital world.</p>
                 <p className='mb-5 text-neutral-700 text-sm'><b>2. Reliability,</b> Stay connected and productive, backed by our robust network and dedicated support.</p>
                 <p className='mb-5 text-neutral-700 text-sm'><b>3. Simple and secure,</b> Enjoy effortless setup, intuitive tools, and advanced security features for peace of mind.</p>
@@ -143,11 +143,22 @@ export default function Home() {
 
             <h2 className='text-primary-700 text-xl font-semibold mb-5 text-center sm:text-left'>Ideanet as a Service(IaaS)</h2>
             <div className="md:grid md:grid-cols-2 mb-5">
-              <Image src={'/assets/home_product2.svg'} alt={'solution for retail'} width={1000} height={1000} className='object-cover mx-auto w-3/4 mb-5' />
-              <div id='cardproduct' className="bg-white rounded-2xl py-10 px-8 w-4/5 sm:w-full mb-10 mx-auto shadow-[0_0_10px_3px_rgba(229,229,229)] h-min">
+              <Image src={'/assets/home_product2.webp'} alt={'solution for retail'} width={1000} height={1000} className='object-cover mx-auto w-3/4 mb-5' />
+              <div id='cardproduct' className="items-center bg-white rounded-2xl py-10 px-8 w-4/5 sm:w-full md:mt-10 mb-10 mx-auto shadow-[0_0_10px_3px_rgba(229,229,229)] h-min">
                 <p className='mb-5 text-neutral-700 text-sm'><b>1. Smart Parking,</b>  Optimize parking management with real-time data insights, automated guidance, and seamless payment integrations.</p>
                 <p className='mb-5 text-neutral-700 text-sm'><b>2. Smart Cluster,</b> Build secure and scalable computing clusters for high-performance applications like data analytics, and scientific research.</p>
                 <p className='mb-5 text-neutral-700 text-sm'><b>3. Smart Healthcare,</b> Revolutionize healthcare delivery with connected devices, remote monitoring, and advanced data analysis tools.</p>
+                <button className="btn btn-outline btn-secondary btn-sm">More info</button>
+              </div>
+            </div>
+
+            <h2 className='text-primary-700 text-xl font-semibold mb-5 text-center sm:text-left'>SDWAN Connectivity</h2>
+            <div className="md:grid md:grid-cols-2 mb-5">
+              <Image src={'/assets/home_product3.webp'} alt={'solution for retail'} width={500} height={600} className='object-cover mx-auto w-3/4 mb-5 sm:order-2' />
+              <div id='cardproduct' className="bg-white rounded-2xl py-10 px-8 w-4/5 sm:w-full md:mt-10 mb-10 sm:order-1 mx-auto shadow-[0_0_10px_3px_rgba(229,229,229)] h-min">
+                <p className='mb-5 text-neutral-700 text-sm'><b>1. Easy Deployment,</b> Get up and running quickly with minimal disruption to your business.</p>
+                <p className='mb-5 text-neutral-700 text-sm'><b>2. 24/7 Support,</b> Our team is here to support you, ensuring your network runs smoothly around the clock.</p>
+                <p className='mb-5 text-neutral-700 text-sm'><b>3. Customizable Options,</b> Tailor your SD-WAN solution to fit your specific needs and objectives.</p>
                 <button className="btn btn-outline btn-secondary btn-sm">More info</button>
               </div>
             </div>
@@ -157,30 +168,32 @@ export default function Home() {
 
       <section id='partner'>
         <div className="flex flex-col justify-center items-center mb-10">
-          <h2 className='text-neutral-700 font-semibold text-center text-2xl w-4/5 lg:w-1/2 mb-10'>A diverse range of partners, From individual to industry giants, we empower companies across every field.</h2>
+          <h2 className='text-neutral-700 font-semibold text-center text-2xl w-4/5 lg:w-1/2 mb-10'>A diverse range of partners, From individual to giants enterprise, we empower companies across every field.</h2>
           <div className="container sm:w-4/5">
             <div className="carousel carousel-end rounded-box">
               <div className="carousel-item">
-              {/* <Image src="/assets/home_cover.jpg" alt={'home cover'} width={500} height={250} className='object-cover' /> */}
-                <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" />
+                <Image src="/assets/client1.webp" alt={'ciputra'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" />
+                <Image src="/assets/client2.webp" alt={'milcit'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" />
+                <Image src="/assets/client3.webp" alt={'pakuwon'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" />
+                <Image src="/assets/client4.webp" alt={'anandamaya'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
+                <Image src="/assets/client5.webp" alt={'pakubuwono menteng'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" />
+                <Image src="/assets/client6.webp" alt={'itdc'} width={200} height={200} className='object-cover' />
               </div>
               <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" />
+                <Image src="/assets/client7.webp" alt={'pakubuwono spring'} width={200} height={200} className='object-cover' />
+              </div>
+              <div className="carousel-item">
+                <Image src="/assets/client8.webp" alt={'mcc'} width={200} height={200} className='object-cover' />
               </div>
             </div>
           </div>
@@ -189,7 +202,7 @@ export default function Home() {
 
       <section>
         <div className="flex flex-col justify-center items-center mb-10 mx-auto">
-          <h2 className='text-primary-800 font-semibold text-3xl sm:text-4xl text-center w-4/5 mb-10'>Ready to experience the future of connection? Join Ideanet, your digital partner in success.</h2>
+          <h2 className='text-primary-800 font-semibold text-3xl sm:text-4xl text-center w-4/5 mb-10'>Ready to experience the future of connection?</h2>
           <Link href={'https://daftar.ideanet.net.id/'} className="btn btn-primary btn-wide ">Subscribe Now</Link>
 
         </div>
