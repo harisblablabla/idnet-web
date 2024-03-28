7d77c294dc661abce1c6e2e15aab49d38e896cd7
haris <haris.fadhilah21@gmail.com>
2024-03-28 14:42:31 +0700

    feat: finished translate to id homepage

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -12,4 +16,5 @@ const nextConfig = {
       },
 }
 
-module.exports = nextConfig
+
+module.exports = withNextIntl(nextConfig)
